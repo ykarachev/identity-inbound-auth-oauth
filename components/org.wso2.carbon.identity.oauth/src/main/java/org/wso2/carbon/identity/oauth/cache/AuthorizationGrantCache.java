b1519c0f58bead0978a3ced35f1704d00090e4a4
Pushpalanka <lanka@wso2.com>
2016-03-30 12:51:30 +0530

    Fixing IDENTITY-4449 with the fix provided by Asela

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/cache/AuthorizationGrantCache.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/cache/AuthorizationGrantCache.java
@@ -22,7 +22,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.store.SessionDataStore;
 import org.wso2.carbon.identity.application.common.cache.BaseCache;
-import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
 import org.wso2.carbon.identity.oauth2.dao.TokenMgtDAO;
 import org.wso2.carbon.utils.CarbonUtils;
@@ -121,7 +120,7 @@ public class AuthorizationGrantCache extends BaseCache<AuthorizationGrantCacheKe
     public AuthorizationGrantCacheEntry getValueFromCacheByCode(AuthorizationGrantCacheKey key) {
         AuthorizationGrantCacheEntry cacheEntry = super.getValueFromCache(key);
         if (cacheEntry == null) {
-            getFromSessionStore(replaceFromCodeId(key.getUserAttributesId()));
+            cacheEntry = getFromSessionStore(replaceFromCodeId(key.getUserAttributesId()));
         }
         return cacheEntry;
     }

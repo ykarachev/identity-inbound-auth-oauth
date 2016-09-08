da05a017b3c2c019abf6b005aaef973317dc6e1e
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2016-09-08 10:33:18 +0530

    Fixing IDENTITY-4698

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/handlers/grant/RefreshGrantHandler.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/handlers/grant/RefreshGrantHandler.java
@@ -99,14 +99,22 @@ public class RefreshGrantHandler extends AbstractAuthorizationGrantHandler {
                 validationDataDO.getAuthorizedUser(),
                 userStoreDomain, OAuth2Util.buildScopeString(validationDataDO.getScope()), true);
 
-        if (accessTokenDO == null){
-            if(log.isDebugEnabled()){
+        if (accessTokenDO == null) {
+            if (log.isDebugEnabled()) {
                 log.debug("Error while retrieving the latest refresh token");
             }
+            if (cacheEnabled) {
+                clearCache(tokenReqDTO.getClientId(), validationDataDO.getAuthorizedUser().toString(),
+                        validationDataDO.getScope(), validationDataDO.getAccessToken());
+            }
             return false;
-        }else if(!refreshToken.equals(accessTokenDO.getRefreshToken())){
-            if(log.isDebugEnabled()){
+        } else if (!refreshToken.equals(accessTokenDO.getRefreshToken())) {
+            if (log.isDebugEnabled()) {
                 log.debug("Refresh token is not the latest.");
+            }
+            if (cacheEnabled) {
+                clearCache(tokenReqDTO.getClientId(), validationDataDO.getAuthorizedUser().toString(),
+                        validationDataDO.getScope(), validationDataDO.getAccessToken());
             }
             return false;
         }
@@ -255,7 +263,11 @@ public class RefreshGrantHandler extends AbstractAuthorizationGrantHandler {
 	    tokenMgtDAO.invalidateAndCreateNewToken(oldAccessToken.getTokenId(), OAuthConstants.TokenStates.TOKEN_STATE_INACTIVE, clientId,
 	                                            UUID.randomUUID().toString(), accessTokenDO,
 	                                            userStoreDomain);
-
+        if (!accessToken.equals(accessTokenDO.getAccessToken())) {
+            // Using latest active token.
+            accessToken = accessTokenDO.getAccessToken();
+            refreshToken = accessTokenDO.getRefreshToken();
+        }
         //remove the previous access token from cache and add the new access token info to the cache,
         // if it's enabled.
         if (cacheEnabled) {
@@ -330,5 +342,25 @@ public class RefreshGrantHandler extends AbstractAuthorizationGrantHandler {
         tokenRespDTO.setErrorCode(errorCode);
         tokenRespDTO.setErrorMsg(errorMsg);
         return tokenRespDTO;
+    }
+
+
+    private void clearCache(String clientId, String authorizedUser, String[] scopes, String accessToken) {
+
+        boolean isUsernameCaseSensitive = IdentityUtil.isUserStoreInUsernameCaseSensitive(authorizedUser);
+        String cacheKeyString;
+        if (isUsernameCaseSensitive) {
+            cacheKeyString = clientId + ":" + authorizedUser + ":" + OAuth2Util.buildScopeString(scopes);
+        } else {
+            cacheKeyString = clientId + ":" + authorizedUser.toLowerCase() + ":" + OAuth2Util.buildScopeString(scopes);
+        }
+
+        // Remove the old access token from the OAuthCache
+        OAuthCacheKey oauthCacheKey = new OAuthCacheKey(cacheKeyString);
+        oauthCache.clearCacheEntry(oauthCacheKey);
+
+        // Remove the old access token from the AccessTokenCache
+        OAuthCacheKey accessTokenCacheKey = new OAuthCacheKey(accessToken);
+        oauthCache.clearCacheEntry(accessTokenCacheKey);
     }
 }

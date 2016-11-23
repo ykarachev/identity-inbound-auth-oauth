f0e2e5009aaadd73722e0a5e2c6947d2168aab45
GDRDABARERA <gdrdabarera@gmail.com>
2016-11-23 16:19:43 +0530

    IDENTITY-5325: Fixed revoking process of a OAuth/OIDC app

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/cache/SessionDataCache.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth/cache/SessionDataCache.java
@@ -23,6 +23,8 @@ import org.wso2.carbon.identity.application.common.cache.BaseCache;
 import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.utils.CarbonUtils;
 
+import javax.cache.Cache;
+
 // Cache used by Authorization endpoint. This class cannot be in oauth.endpoint component
 // since it needs to be visible to Hazelcast.
 public class SessionDataCache extends BaseCache<SessionDataCacheKey, SessionDataCacheEntry> {

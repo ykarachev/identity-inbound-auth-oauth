317f8e580aca2a15794cf873a81b99c8c50a8398
denuwanthi <denuwanthi.hasanthika@gmail.com>
2017-04-25 14:23:38 +0530

    Format redundant code

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/handlers/grant/AuthorizationCodeGrantHandler.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/handlers/grant/AuthorizationCodeGrantHandler.java
@@ -99,11 +99,11 @@ public class AuthorizationCodeGrantHandler extends AbstractAuthorizationGrantHan
             authzCodeDO = tokenMgtDAO.validateAuthorizationCode(clientId, authorizationCode);
         }
 
-        if (authzCodeDO != null && OAuthConstants.AuthorizationCodeState.INACTIVE.equals(authzCodeDO.getState())){
-            String scope = OAuth2Util.buildScopeString(authzCodeDO.getScope());
-            String authorizedUser = authzCodeDO.getAuthorizedUser().toString();
-            boolean isUsernameCaseSensitive = IdentityUtil.isUserStoreInUsernameCaseSensitive(authorizedUser);
+        if (authzCodeDO != null && OAuthConstants.AuthorizationCodeState.INACTIVE.equals(authzCodeDO.getState())) {
             if (cacheEnabled) {
+                String scope = OAuth2Util.buildScopeString(authzCodeDO.getScope());
+                String authorizedUser = authzCodeDO.getAuthorizedUser().toString();
+                boolean isUsernameCaseSensitive = IdentityUtil.isUserStoreInUsernameCaseSensitive(authorizedUser);
                 String cacheKeyString;
                 if (isUsernameCaseSensitive) {
                     cacheKeyString = clientId + ":" + authorizedUser + ":" + scope;

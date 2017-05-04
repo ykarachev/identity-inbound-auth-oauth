925d7f68755ae9b5a86aba758c352adcaaff2def
shariq <msmshariq@gmail.com>
2017-05-04 14:15:57 +0530

    Update method signature

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/service/DefaultKeyProvider.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/service/DefaultKeyProvider.java
@@ -81,7 +81,7 @@ public class DefaultKeyProvider implements KeyProviderService {
     }
 
     @Override
-    public Certificate getCertificate(String s) throws Exception {
+    public Certificate getCertificate(String tenantDomain, String alias) throws Exception {
         return null;
     }
 }

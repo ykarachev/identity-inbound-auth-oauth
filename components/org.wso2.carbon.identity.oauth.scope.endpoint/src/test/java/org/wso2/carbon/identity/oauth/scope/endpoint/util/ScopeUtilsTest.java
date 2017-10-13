bdffd50982685d5f0ee54b38f47a11a163983c05
isuri97 <isurianuradha96@gmail.com>
2017-10-13 10:08:45 +0530

    added unittest for scope endpoint

--- components/org.wso2.carbon.identity.oauth.scope.endpoint/src/test/java/org/wso2/carbon/identity/oauth/scope/endpoint/util/ScopeUtilsTest.java
+++ components/org.wso2.carbon.identity.oauth.scope.endpoint/src/test/java/org/wso2/carbon/identity/oauth/scope/endpoint/util/ScopeUtilsTest.java
@@ -18,104 +18,148 @@
 
 package org.wso2.carbon.identity.oauth.scope.endpoint.util;
 
-
-import org.testng.Assert;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mockito.Mock;
+import org.osgi.framework.BundleContext;
+import org.osgi.util.tracker.ServiceTracker;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.IObjectFactory;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.ObjectFactory;
 import org.testng.annotations.Test;
+import org.wso2.carbon.identity.oauth.scope.endpoint.Exceptions.ScopeEndpointException;
 import org.wso2.carbon.identity.oauth.scope.endpoint.dto.ErrorDTO;
 import org.wso2.carbon.identity.oauth.scope.endpoint.dto.ScopeDTO;
 import org.wso2.carbon.identity.oauth.scope.endpoint.dto.ScopeToUpdateDTO;
+import org.wso2.carbon.identity.oauth2.IdentityOAuth2ScopeException;
+import org.wso2.carbon.identity.oauth2.OAuth2ScopeService;
 import org.wso2.carbon.identity.oauth2.bean.Scope;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.core.Response;
 
 import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 
+public class ScopeUtilsTest extends PowerMockTestCase {
 
-public class ScopeUtilsTest {
+    private final String CLIENT_NAME = "clientname";
+    private final String CODE = "AJYRKLWB68NSB9";
+    private final String MESSAGE = "Lifecycle exception occurred";
+    private final String DESCRIPTION = "Error occurred while changing lifecycle state";
 
+    private static final Log log = LogFactory.getLog(ScopeUtilsTest.class);
+
+    @Mock
+    BundleContext bundleContext;
+
+    @Mock
+    ServiceTracker serviceTracker;
+
+    @Mock
+    private IdentityOAuth2ScopeException identityOAuth2ScopeException;
+
+    @Mock
+    OAuth2ScopeService service;
+
+    @ObjectFactory
+    public IObjectFactory getObjectFactory() {
+        return new org.powermock.modules.testng.PowerMockObjectFactory();
+    }
 
     @Test(description = "Testing getErrorDTO")
     public void testGetErrorDTO() throws Exception {
-
         ErrorDTO errorDTOexpected = new ErrorDTO();
-        errorDTOexpected.setCode("AJYRKLWB68NSB9");
-        errorDTOexpected.setMessage("Lifecycle exception occurred");
-        errorDTOexpected.setDescription("Error occurred while changing lifecycle state");
+        errorDTOexpected.setCode(CODE);
+        errorDTOexpected.setMessage(MESSAGE);
+        errorDTOexpected.setDescription(DESCRIPTION);
 
-        ErrorDTO errorDTO1 = ScopeUtils.getErrorDTO("Lifecycle exception occurred", "AJYRKLWB68NSB9", "Error occurred while changing lifecycle state");
-        Assert.assertEquals(errorDTO1.getCode(), errorDTOexpected.getCode());
-        Assert.assertEquals(errorDTO1.getMessage(), errorDTOexpected.getMessage());
-        Assert.assertEquals(errorDTO1.getDescription(), errorDTOexpected.getDescription());
-
+        ErrorDTO errorDTO1 = ScopeUtils.getErrorDTO(MESSAGE, CODE, DESCRIPTION);
+        assertEquals(errorDTO1.getCode(), errorDTOexpected.getCode(), "Actual code is not match for expected code");
+        assertEquals(errorDTO1.getMessage(), errorDTOexpected.getMessage(), "Actual message is not match for expected message");
+        assertEquals(errorDTO1.getDescription(), errorDTOexpected.getDescription(), "Actual description is not match for expected description");
     }
 
     @Test(description = "Testing getScope")
     public void testGetScope() throws Exception {
 
-        ScopeDTO scopeDTO = mock(ScopeDTO.class);
-        ArrayList<String> scope = new ArrayList<>();
-
-        when(scopeDTO.getBindings()).thenReturn(scope);
-        when(scopeDTO.getName()).thenReturn("Lifecycle exception occurred");
-        when(scopeDTO.getDescription()).thenReturn("Error occurred while changing lifecycle state");
-
-        Scope scopeexpected = new Scope("Error occurred while changing lifecycle state", "Lifecycle exception occurred", new ArrayList<String>(Arrays.asList("scope1", "scope2")));
-        scopeexpected.setName("Lifecycle exception occurred");
-        scopeexpected.setDescription("Error occurred while changing lifecycle state");
-        scopeexpected.setBindings(scope);
+        ScopeDTO scopeDTO = new ScopeDTO();
+        scopeDTO.setName("clientname");
+        scopeDTO.setDescription("Error occurred while changing lifecycle state");
+        ArrayList binding = new ArrayList();
 
         Scope scope1 = ScopeUtils.getScope(scopeDTO);
-        Assert.assertEquals(scope1.getName(), scopeexpected.getName());
-        Assert.assertEquals(scope1.getDescription(), scopeexpected.getDescription());
-        Assert.assertEquals(scope1.getBindings(), scopeexpected.getBindings());
-
+        assertEquals(scope1.getName(), CLIENT_NAME, "Actual name is not match for expected name");
+        assertEquals(scope1.getDescription(),DESCRIPTION, "Actual description is not match for expected description");
+        assertEquals(scope1.getBindings(), binding, "Actual binding is not match for expected binding");
     }
 
     @Test(description = "Testing getScopeDTO")
     public void testGetScopeDTO() throws Exception {
-
-        Scope scope = mock(Scope.class);
-        when(scope.getBindings()).thenReturn(new ArrayList<String>());
-        when(scope.getDescription()).thenReturn("Lifecycle exception occurred");
-        when(scope.getName()).thenReturn("Error occurred while changing lifecycle state");
-
-        ScopeDTO scopeDTOexpected = new ScopeDTO();
-        scopeDTOexpected.setBindings(new ArrayList<String>());
-        scopeDTOexpected.setDescription("Lifecycle exception occurred");
-        scopeDTOexpected.setName("Error occurred while changing lifecycle state");
+        ArrayList bindings = new ArrayList();
+        bindings.add("binding1");
+        Scope scope = new Scope(CLIENT_NAME, DESCRIPTION, bindings);
 
         ScopeDTO scopeDTO1 = ScopeUtils.getScopeDTO(scope);
-        Assert.assertEquals(scopeDTO1.getBindings(), scopeDTOexpected.getBindings());
-        Assert.assertEquals(scopeDTO1.getDescription(), scopeDTOexpected.getDescription());
-        Assert.assertEquals(scopeDTO1.getName(), scopeDTOexpected.getName());
-
+        assertEquals(scopeDTO1.getBindings(), bindings,"Actual binding is not match for expected binding");
+        assertTrue(scopeDTO1.getBindings().get(0).contains("binding1"));
+        assertEquals(scopeDTO1.getDescription(), DESCRIPTION, "Actual description is not match for expected description");
+        assertEquals(scopeDTO1.getName(), CLIENT_NAME, "Actual name is not match for expected name");
     }
 
     @Test(description = "Testing getUpdateScope")
     public void testGetUpdatedScope() throws Exception {
 
-        ScopeToUpdateDTO scopeToUpdateDTO = mock(ScopeToUpdateDTO.class);
-        ArrayList<String> scope = new ArrayList<>(Arrays.asList("scope1", "scope2"));
-        when(scopeToUpdateDTO.getDescription()).thenReturn("Error occurred while changing lifecycle state");
-        when(scopeToUpdateDTO.getBindings()).thenReturn(scope);
-
-        Scope scopeexpected = new Scope("openid", "Error occurred while changing lifecycle state", scope);
-        scopeexpected.setDescription("Error occurred while changing lifecycle state");
-        scopeexpected.setBindings(scope);
         ScopeToUpdateDTO sc = new ScopeToUpdateDTO();
+        ArrayList bindings = new ArrayList();
+        sc.setDescription("Error occurred while changing lifecycle state");
 
-        Scope scope1 = ScopeUtils.getUpdatedScope(scopeToUpdateDTO, "openid");
-        Assert.assertEquals(scope1.getDescription(), scopeexpected.getDescription());
-        Assert.assertEquals(scope1.getBindings(), scopeexpected.getBindings());
+        Scope scope1 = ScopeUtils.getUpdatedScope(sc, "Actual name is not match for expected name");
+        assertEquals(scope1.getBindings(), bindings,"Actual binding is not match for expected binding");
+        assertEquals(scope1.getDescription(), DESCRIPTION, "Actual description is not match for expected description");
 
     }
 
-    @Test
+    @Test(description = "Testing getScopeDTO")
     public void testGetScopeDTOs() throws Exception {
-
-
+        int scopeName;
+        int scopeSize = 15;
+        Set<Scope> scopes = new HashSet<>();
+        ArrayList<String> bindings = new ArrayList<>(Arrays.asList("scope1", "scope2"));
+        for (int i = 0; i < scopeSize; i++) {
+            Scope scope1 = new Scope(CLIENT_NAME + "" + i, DESCRIPTION, bindings);
+            scopes.add(scope1);
+        }
+        Set<ScopeDTO> scopeDTOs = ScopeUtils.getScopeDTOs(scopes);
+        assertNotNull(scopeDTOs);
+        assertEquals(scopeDTOs.size(), scopeSize, "Scopes can't be found");
     }
 
+    @DataProvider(name = "BuildScopeEndpointException")
+    public Object[][] buildScopeEndpointException() {
+        Response.Status status = Response.Status.BAD_REQUEST;
+        Throwable throwable1 = new ScopeEndpointException(status);
+        Throwable throwable2 = new RuntimeException("BAD_REQUEST_INVALID_REDIRECT_URI");
+        return new Object[][]{
+                {status,throwable1, true},
+                {status,throwable1, false},
+                {status,throwable2, true},
+                {status,throwable2, false},
+                {status,throwable1, true}
+        };
+    }
+
+    @Test(dataProvider = "BuildScopeEndpointException", expectedExceptions = ScopeEndpointException.class)
+    public void testHandleErrorResponse(Response.Status status,Throwable throwable, boolean isServerException) throws Exception {
+        String message = "Scope";
+        ScopeUtils.handleErrorResponse(status, message, throwable, isServerException,log);
+    }
 }
+

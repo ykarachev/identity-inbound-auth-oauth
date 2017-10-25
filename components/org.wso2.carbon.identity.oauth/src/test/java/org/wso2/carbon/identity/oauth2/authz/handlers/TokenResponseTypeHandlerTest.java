1369229d222ea4bbf4e1cb6c5d8b35063537a341
dharshanaw <dkasunw@gmail.com>
2017-10-25 13:46:23 +0530

    Adding tests for TokenResponseTypeHandler

--- components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/authz/handlers/TokenResponseTypeHandlerTest.java
+++ components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/authz/handlers/TokenResponseTypeHandlerTest.java
@@ -1,5 +1,22 @@
-package org.wso2.carbon.identity.oauth2.authz.handlers;
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
+package org.wso2.carbon.identity.oauth2.authz.handlers;
 
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -32,7 +49,6 @@ import static org.mockito.Matchers.eq;
 /**
  * Unit test covering TokenResponseTypeHandler class
  */
-
 @PrepareForTest({OAuthComponentServiceHolder.class, OAuthEventInterceptor.class, IdentityUtil.class, OAuth2Util.class})
 public class TokenResponseTypeHandlerTest extends PowerMockTestCase {
     @BeforeMethod
@@ -62,7 +78,6 @@ public class TokenResponseTypeHandlerTest extends PowerMockTestCase {
                 OAuthServerConfiguration.class.getDeclaredField("instance");
         oAuthServerConfigInstance.setAccessible(true);
         oAuthServerConfigInstance.set(null, oAuthServerConfiguration);
-
     }
 
     @Test

1a99d5d5aeb3e0e0110097c4c6eca3d113c3b716
Ruwan Abeykoon <ruwana@wso2.com>
2017-10-30 09:42:05 +0530

    Added some common test utilities as annotations.

---
+++ components/org.wso2.carbon.identity.oauth/src/test/java/org/wso2/carbon/identity/oauth2/validators/JDBCScopeValidatorTest.java
@@ -0,0 +1,39 @@
+package org.wso2.carbon.identity.oauth2.validators;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.wso2.carbon.identity.oauth2.model.AccessTokenDO;
+
+import static org.testng.Assert.*;
+
+/**
+ * Tests JDBCScopeValidator.
+ */
+public class JDBCScopeValidatorTest {
+
+    private JDBCScopeValidator validator;
+
+    @BeforeTest
+    public void setUp() {
+        validator = new JDBCScopeValidator();
+    }
+
+    @Test(dataProvider = "validatingDOs")
+    public void testValidateScope(AccessTokenDO accessTokenDO, String scope, boolean expectedResult) throws Exception {
+        boolean result = validator.validateScope(accessTokenDO, scope);
+        assertEquals(result, expectedResult);
+    }
+
+
+    @DataProvider(name = "validatingDOs")
+    public Object[][] createValidateTokenDo() {
+
+        AccessTokenDO accessTokenDO = new AccessTokenDO();
+
+        return new Object[][]{
+                {accessTokenDO, "scope1", true}
+        };
+    }
+
+}
